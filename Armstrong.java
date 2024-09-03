57e8083e06c7a19b42551431b416cce61841adca
Krishanzzz <156848293+Krishanzzz@users.noreply.github.com>
2024-09-03 13:54:01 +0530

    Create Armstrong.java
    To find the given number is armstrong or not

---
+++ Armstrong.java
@@ -0,0 +1,27 @@
+package Samples;
+
+import java.util.Scanner;
+
+public class Armstrong {
+
+	public static void main(String[] args) {
+		int tot=0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int n = sc.nextInt(); 
+		int temp = n;
+		int len = String.valueOf(temp).length();
+		while(n!=0) {
+			int digit = n%10;
+			tot += Math.pow(digit,len);
+			n/=10;
+		}
+		if(temp == tot) {
+			System.out.println("This "+temp+" is armstrong number");
+		}else {
+			System.out.println("This "+temp+" is not armstrong number");
+		}
+		sc.close();
+	}
+
+}
